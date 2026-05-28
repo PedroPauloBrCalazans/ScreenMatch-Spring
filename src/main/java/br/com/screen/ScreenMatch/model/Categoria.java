a56f7a966efe0b00433ea62dd3af8d8075c94989
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-05-28 00:41:59 -0300

    FEAT: Finalizando a implementação da modelagem de categorias e séries.

--- src/main/java/br/com/screen/ScreenMatch/model/Categoria.java
+++ src/main/java/br/com/screen/ScreenMatch/model/Categoria.java
@@ -1,4 +1,24 @@
 package br.com.screen.ScreenMatch.model;
 
 public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: ");
+    } //se o texto que está vindo e igual a categoria do omdb, atribui a nossa categoria
 }
--- src/main/java/br/com/screen/ScreenMatch/model/Serie.java
+++ src/main/java/br/com/screen/ScreenMatch/model/Serie.java
@@ -1,5 +1,7 @@
 package br.com.screen.ScreenMatch.model;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
 
@@ -14,4 +16,14 @@ public class Serie {
     private String poster;
 
     private String sinopse;
+
+    public Serie(DadosSerie dadosSerie) {
+        this.titulo = dadosSerie.titulo();
+        this.totalTemp = dadosSerie.totalTemp();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
 }
