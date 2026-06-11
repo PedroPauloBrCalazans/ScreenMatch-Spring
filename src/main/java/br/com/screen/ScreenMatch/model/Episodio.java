e7e2297bdb22e2165be63fa161482d02fab879b4
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-06-11 00:31:12 -0300

    FEAT: Implementação do relacionamento entre Série e Episodios.

--- src/main/java/br/com/screen/ScreenMatch/model/Episodio.java
+++ src/main/java/br/com/screen/ScreenMatch/model/Episodio.java
@@ -1,15 +1,30 @@
 package br.com.screen.ScreenMatch.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
+@Entity
+@Table(name = "episodios")
 public class Episodio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Integer temporada;
+
     private String titulo;
+
     private Integer numeroEpisodio;
+
     private Double avaliacao;
+
     private LocalDate dataLancamento;
+
+    @ManyToOne
+    private Serie serie;
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
@@ -27,6 +42,10 @@ public class Episodio {
         } catch (DateTimeParseException ex) {
             this.dataLancamento = null;
         }
+    }
+
+    public Episodio() {
+
     }
 
     public Integer getTemporada() {
@@ -67,6 +86,22 @@ public class Episodio {
 
     public void setDataLancamento(LocalDate dataLancamento) {
         this.dataLancamento = dataLancamento;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     @Override
