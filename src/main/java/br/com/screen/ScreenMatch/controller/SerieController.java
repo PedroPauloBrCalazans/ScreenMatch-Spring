e30bd9f051c27b7e536056946936bd1e8c1f49f8
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-06-29 00:58:37 -0300

    FEAT: Representando séries do banco com DTO + ajustes na rota GET.

--- src/main/java/br/com/screen/ScreenMatch/controller/SerieController.java
+++ src/main/java/br/com/screen/ScreenMatch/controller/SerieController.java
@@ -1,6 +1,6 @@
 package br.com.screen.ScreenMatch.controller;
 
-import br.com.screen.ScreenMatch.model.Serie;
+import br.com.screen.ScreenMatch.dto.SerieDTO;
 import br.com.screen.ScreenMatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,11 @@ public class SerieController {
     private SerieRepository repository;
 
     @GetMapping("/series")
-    public List<Serie> obterSeries() {
-        return repository.findAll();
+    public List<SerieDTO> obterSeries() {
+        return repository.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(),
+                        s.getTotalTemp(), s.getAvaliacao(), s.getGenero(),
+                        s.getAtores(), s.getPoster(), s.getSinopse())).toList();
     }
 }
