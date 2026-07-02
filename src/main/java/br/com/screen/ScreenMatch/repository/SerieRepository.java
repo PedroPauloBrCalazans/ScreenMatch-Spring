92260b6aa9c833c0ef7b4a3213a0e752a88b312e
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-07-02 01:21:55 -0300

    FEAT: Implementação da QUERY encontrarEpisodiosMaisRecentes utilizando JPQL.

--- src/main/java/br/com/screen/ScreenMatch/repository/SerieRepository.java
+++ src/main/java/br/com/screen/ScreenMatch/repository/SerieRepository.java
@@ -35,6 +35,12 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
     List<Episodio> episodiosPorSerieEAno(Serie serie, int anoLancamento);
 
     List<Serie> findTop5ByOrderByEpisodiosDataLancamentoDesc();
+
+//    @Query("SELECT s FROM Serie s " +
+//            "JOIN s.episodios e " +
+//            "GROUP BY s " +
+//            "ORDER BY MAX(e.dataLancamento) DESC LIMIT 5")
+//    List<Serie> encontrarEpisodiosMaisRecentes(); UTILIZANDO JPQL DO MESMO MÉTODO findTop5ByOrderByEpisodiosDataLancamentoDesc
 }
 
 //No 1° parametro da JpaRepository passar a entidade que estou manipulando, 2° o ID
